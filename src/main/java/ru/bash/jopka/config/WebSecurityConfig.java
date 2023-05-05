537d6e7a7488f5c21ba4a2f828625ffffdde4268
Сулейманов Сулейман Айдарович <ssuleimanov@sberbank-tele.com>
2023-05-05 14:11:06 +0500

    new: cors

--- src/main/java/ru/bash/jopka/config/WebSecurityConfig.java
+++ src/main/java/ru/bash/jopka/config/WebSecurityConfig.java
@@ -14,6 +14,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import ru.bash.jopka.security.jwt.JwtAuthenticationEntryPoint;
 import ru.bash.jopka.security.jwt.JwtAuthenticationFilter;
 
@@ -21,7 +23,7 @@ import ru.bash.jopka.security.jwt.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableMethodSecurity
 @RequiredArgsConstructor
-public class WebSecurityConfig {
+public class WebSecurityConfig implements WebMvcConfigurer {
     private final JwtAuthenticationEntryPoint authenticationEntryPoint;
     private final JwtAuthenticationFilter authenticationFilter;
 
@@ -34,6 +36,11 @@ public class WebSecurityConfig {
     @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
         return configuration.getAuthenticationManager();
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("*");
     }
 
     @Bean
