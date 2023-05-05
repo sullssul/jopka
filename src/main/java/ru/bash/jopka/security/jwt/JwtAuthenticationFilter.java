ff2fbf616e163bf9ec27d9d54eba1274b4b7292c
Сулейманов Сулейман Айдарович <ssuleimanov@sberbank-tele.com>
2023-05-05 16:04:36 +0500

    new: cors

--- src/main/java/ru/bash/jopka/security/jwt/JwtAuthenticationFilter.java
+++ src/main/java/ru/bash/jopka/security/jwt/JwtAuthenticationFilter.java
@@ -50,16 +50,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
             authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
             SecurityContextHolder.getContext().setAuthentication(authenticationToken);
         }
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, xsrf-token");
-        response.addHeader("Access-Control-Expose-Headers", "xsrf-token");
-        if ("OPTIONS".equals(request.getMethod())) {
-            response.setStatus(HttpServletResponse.SC_OK);
-        } else {
-            filterChain.doFilter(request, response);
-        }
+
         filterChain.doFilter(request, response);
     }
 
