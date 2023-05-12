0f6d3965c809e91917dbbb4125fdc16adabd0e5c
Сулейманов Сулейман Айдарович <ssuleimanov@sberbank-tele.com>
2023-05-12 20:35:52 +0500

    new:docker

---
+++ src/test/java/ru/bash/jopka/buisness/nomination/NominationServiceTest.java
@@ -0,0 +1,11 @@
+package ru.bash.jopka.buisness.nomination;
+
+
+import ru.bash.jopka.business.nomination.impl.NominationServiceImpl;
+import ru.bash.jopka.business.nomination.usecase.*;
+
+class NominationServiceTest {
+
+
+
+}
